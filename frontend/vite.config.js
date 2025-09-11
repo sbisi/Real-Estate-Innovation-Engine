5ba2b51d58cf726a252613adc0bcbc3f5f373c0b
sbisi <62792590+sbisi@users.noreply.github.com>
2025-09-11 06:31:32 +0200

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://rei-backend-mtcr.onrender.com',  // Your backend URL
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
 })
+
