3c8816a96cb43db546497d1a79101724aa6a3f79
sbisi <62792590+sbisi@users.noreply.github.com>
2025-09-12 10:52:09 +0200

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
+
