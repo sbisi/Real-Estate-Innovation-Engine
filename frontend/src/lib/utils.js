d9e26916378dbc0efc6da79d8b131b8bb00fbd0d
sbisi <62792590+sbisi@users.noreply.github.com>
2025-09-12 11:02:17 +0200

    Add files via upload

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,12 @@
-import { clsx } from "clsx";
+import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
+
+// Alternative simple version without tailwind-merge
+export function cnSimple(...inputs) {
+  return inputs.filter(Boolean).join(' ')
+}
+
